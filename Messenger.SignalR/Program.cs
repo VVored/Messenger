2a8a413761d8449336533a28c40be16d95584d97
VVored <muckinvova@gmail.com>
2024-12-17 19:49:56 +0500

    Добавьте файлы проекта.

---
+++ Messenger.SignalR/Program.cs
@@ -0,0 +1,10 @@
+using Messenger.SignalR.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
